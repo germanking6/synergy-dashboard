f562d6d3a6ade1b37e5cd2230b663a0a6628aacf
German Jaramillo <65362339+germanking6@users.noreply.github.com>
2023-03-16 16:16:37 -0600

    restructure

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAkrd1-WWfzAEBDKeh2-L7Rj8eu8liaAFY",
-  authDomain: "synergy-52306.firebaseapp.com",
-  databaseURL: "https://synergy-52306-default-rtdb.firebaseio.com",
-  projectId: "synergy-52306",
-  storageBucket: "synergy-52306.appspot.com",
-  messagingSenderId: "764371875112",
-  appId: "1:764371875112:web:c046d12c91e83db0c50c1d",
-  measurementId: "G-T7N01Z0JQR"
+const firebaseConfig = {//add your sensitive nfo
 };
 
 // init firebase
